a432838d83e0ee3bc3ecbc29a71466384a5a4aa9
Aaron VerDow <aaron@verdow.com>
2025-02-18 12:11:11 -0600

    test new package name

--- pkgs/top-level/lua-packages.nix
+++ pkgs/top-level/lua-packages.nix
@@ -83,14 +83,14 @@ rec {
     }:
 
     stdenv.mkDerivation {
-      pname = "awesome-wm-widgets";
+      pname = "awesome-wm-widgets2";
       version = "0-unstable-2024-02-15";
 
       src = fetchFromGitHub {
         owner = "streetturtle";
         repo = "awesome-wm-widgets";
         rev = "2a27e625056c50b40b1519eed623da253d36cc27";
-        hash = "sha256-ejihBFxTtuZl/KJa9BalZ8JISAjn9XWmirK37O169lw=";
+        hash = "sha256-XXXhBFxTtuZl/KJa9BalZ8JISAjn9XWmirK37O169lw=";
       };
 
       installPhase = ''
